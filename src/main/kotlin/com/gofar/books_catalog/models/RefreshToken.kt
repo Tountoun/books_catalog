396ca009c59a33fddb0b5f1b43b3dbe48ef25b13
Tountoun <tountounabela@gmail.com>
2023-07-14 13:28:10 +0000

    Add: refresh token

---
+++ src/main/kotlin/com/gofar/books_catalog/models/RefreshToken.kt
@@ -0,0 +1,16 @@
+package com.gofar.books_catalog.models
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+data class RefreshToken (
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    val id: Long = 0,
+    @Column(name = "token")
+    val refreshValue: String
+)
