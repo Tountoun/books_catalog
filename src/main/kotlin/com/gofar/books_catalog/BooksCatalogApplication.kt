e9440f2e9de6af314367382a64d50a92d8411fbd
Tountoun <tountounabela@gmail.com>
2023-07-14 13:29:37 +0000

    Fix: typo

--- src/main/kotlin/com/gofar/books_catalog/BooksCatalogApplication.kt
+++ src/main/kotlin/com/gofar/books_catalog/BooksCatalogApplication.kt
@@ -1,8 +1,10 @@
 package com.gofar.books_catalog
 
 import com.gofar.books_catalog.validators.BookValidator
+import com.gofar.books_catalog.validators.UserDaoValidator
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class BooksCatalogApplication
@@ -11,6 +13,12 @@ fun main(args: Array<String>) {
 	runApplication<BooksCatalogApplication>(*args)
 }
 
+@Bean
 fun bookValidator(): BookValidator {
 	return BookValidator()
+}
+
+@Bean
+fun userDaoValidator(): UserDaoValidator {
+	return UserDaoValidator()
 }
--- src/main/kotlin/com/gofar/books_catalog/models/User.kt
+++ src/main/kotlin/com/gofar/books_catalog/models/User.kt
@@ -1,20 +1,24 @@
 package com.gofar.books_catalog.models
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import com.gofar.books_catalog.utils.Role
+import jakarta.persistence.*
+import lombok.Builder
 
 @Entity
+@Builder
 data class User(
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO,)
-    val id: Long,
+    val id: Long = 0,
     @Column(name = "username")
-    val username: String,
+    var username: String,
+    @Column(name = "email", unique = true, nullable = false)
+    var email: String,
     @Column(name = "password")
-    val password: String,
+    var password: String,
+    @ElementCollection(targetClass = Role::class, fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    val roles: Set<Role>,
     @Column(name = "age")
-    val age: Int
+    var age: Int
     )
